ba2184ebd6dce2649643a33c1921dafd4af85fd4
Tomas <tomas.ew88@gmail.com>
2024-10-20 15:15:03 +0300

    Remove unnecessary imports

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,4 @@
 import Application from "./application.ts";
-import Connection from "./connection.ts";
-import { LoadBalancerType } from "./loadBalancer.ts";
-import RoundRobin from "./roundRobin.ts";
-
-const connections = new Set<Connection>;
 
 function main() {
   const app = new Application(LoadBalancerType.ROUND_ROBIN);
