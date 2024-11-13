fbc2cd2f9ebf6e2b4eebaf73395879322daddfc8
Tomas <tomas.ew88@gmail.com>
2024-11-13 10:17:53 +0200

    Add number of connections that each client should submit

--- tester-client/main.ts
+++ tester-client/main.ts
@@ -2,21 +2,26 @@ const hostname = "127.0.0.1";
 const port = 8000;
 
 const connectionsCount = 4;
-const duration = .5; // seconds
-
+const throttleSeconds = .5; // seconds
+const numberOfRequests = 10;
 
 async function handleConnection(
   connection: Deno.TcpConn,
   clientNumber: number
 ) {
   let timer = Date.now();
+  let requests = 0;
   while (1) {
-    if (Date.now() - timer < duration * 1000) continue;
+    if (requests >= numberOfRequests) break;
+    if (Date.now() - timer < throttleSeconds * 1000) continue;
     const requestText = `Client: ${clientNumber}`;
     await connection.write(new TextEncoder().encode(requestText));
     timer = Date.now();
     await new Promise(resolve => setTimeout(resolve, 10));
+    requests++;
   }
+
+  connection.close();
 }
 
 async function main() {
